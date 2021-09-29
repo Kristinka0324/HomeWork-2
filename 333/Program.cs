c3103f31b8e12ab55c7a9ef374612ac4de66b7ec
Kristina <kristinka.antipova94@gmail.com>
2021-09-30 02:55:05 +0500

    3 задание

---
+++ 333/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _333
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите число и я определю четное оно или не четное :)");
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечётное");
+
+            }
+        }
+    }   
+}

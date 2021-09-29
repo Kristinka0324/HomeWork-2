018e7866e61cc0db0e785ec4d5863194c293070d
Kristina <kristinka.antipova94@gmail.com>
2021-09-30 02:50:16 +0500

    1задание

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mintemp, maxtemp;  
+
+            Console.WriteLine("Введите число 1");
+
+            mintemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 2");
+
+            maxtemp = double.Parse(Console.ReadLine());
+
+            double result = (mintemp + maxtemp) / 2;
+
+            Console.WriteLine("среднесуточная температура = " + result);
+
+        }
+    }
+}

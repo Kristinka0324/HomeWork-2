edb1ddc067758d40323098aa7f56ce529eb74fbd
Kristina <kristinka.antipova94@gmail.com>
2021-09-30 02:56:45 +0500

    4 задание

---
+++ 444/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _444
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    ИП СЕРВИСНЫЙ ЦЕНТР SHIFT    ");
+
+            Console.WriteLine(" ИИН: 920422300036    RHM: 010100348003");
+
+            Console.WriteLine("            Г.АКТАУ 11-38-36         ");
+
+            DateTime aDate = DateTime.Now; ;
+
+            Console.WriteLine(aDate.ToString("            MM-dd-yyyy H:mm     "));
+
+            Console.WriteLine("***         СМЕННЫЙ ОТЧЕТ Z         ***");
+            Console.WriteLine("***         ОТЧЕТ ПО СЕКЦИЯМ        ***");
+            Console.WriteLine("СЕКЦИЯ №                             01");
+            Console.WriteLine("СЕКЦИЯ 01");
+            Console.WriteLine("КОЛ-ВО:                           1.000");
+            Console.WriteLine("СУММА:                             2500");
+            Console.WriteLine("               * * * * *               ");
+            Console.WriteLine("             ФИСКАЛЬНЫЙ ЧЕК            ");
+
+        }
+    }
+}

91da782b4931f9798fb000254f9d43f4a9cb6c8d
Kristina <kristinka.antipova94@gmail.com>
2021-09-30 02:52:34 +0500

    2 задание

---
+++ 222/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("порядковый номер текущего месяца ->  ");
+            DateTime aDate = DateTime.Now; ;
+            Console.WriteLine(aDate.ToString("MM"));
+
+        }
+    }
+}
